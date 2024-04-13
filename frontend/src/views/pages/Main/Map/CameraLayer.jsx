cac6095b53c27c4d6650a377a0047e0b11e2454b
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2024-04-13 10:37:29 +0700

    add camera to db

--- frontend/src/views/pages/Main/Map/CameraLayer.jsx
+++ frontend/src/views/pages/Main/Map/CameraLayer.jsx
@@ -1,12 +1,49 @@
-import React, { useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { CameraApi } from '~/api';
+import AppContext from '~/context';
+import { ErrorService } from '~/services';
+import CameraVer from '~/assets/images/camera/type=ver.svg?react';
+import Camera360 from '~/assets/images/camera/type=360.svg?react';
+import CameraHori from '~/assets/images/camera/type=hori.svg?react';
+import { CameraPoint } from './style';
+import { CameraLocations } from './data';
+function CameraLayer({ zone }) {
+  const { actions } = useContext(AppContext);
+  const isMounted = useRef(false);
+  const [data, setData] = useState([]);
 
-function CameraLayer({}) {
-  const draggableRef = useRef(null);
-  useDraggable(draggableRef);
+  const callApi = async () => {
+    try {
+      const api = await CameraApi.getByFilter({ zone });
+      console.log(api);
+      setData(api.data);
+      isMounted.current = true;
+    } catch (error) {
+      ErrorService.hanldeError(error, actions.onNoti);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  return <div>
-    
-  </div>;
+  useEffect(() => {
+    callApi();
+  }, [zone]);
+
+  const DefaultCameraLocation = CameraLocations[zone] || [];
+  return (
+    <div>
+      {data.map((camera, ix) => {
+        console.log(camera, DefaultCameraLocation);
+        const defaultLocation =
+          DefaultCameraLocation.find((el) => el.cameraId === camera.cameraId)?.location || {};
+        return (
+          <CameraPoint key={'camera' + ix} style={{ position: 'absolute', ...defaultLocation }}>
+            <CameraVer />
+          </CameraPoint>
+        );
+      })}
+    </div>
+  );
 }
 
 export default CameraLayer;
---
+++ frontend/src/views/pages/Main/Map/data.js
@@ -0,0 +1,18 @@
+export const CameraLocations = {
+  A: [
+    {
+      cameraId: 'CAM_001',
+      location: {
+        top: 116,
+        left: 464
+      }
+    },
+    {
+      cameraId: 'CAM_002',
+      location: {
+        top: 54,
+        left: 12
+      }
+    },
+  ]
+};
