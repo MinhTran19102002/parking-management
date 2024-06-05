ea238dc635e53f7dbc3360665b40c9223ddba137
MinhTran19102002 <20110677@student.hcmute.edu.vn>
2024-06-05 20:53:41 +0700

    Api Active

--- backend/src/services/vehicleService.js
+++ backend/src/services/vehicleService.js
@@ -1,4 +1,4 @@
-import { userModel } from '~/models/personModel';
+import { personModel, userModel } from '~/models/personModel';
 import ApiError from '~/utils/ApiError';
 import { parkingModel } from '~/models/parkingModel';
 import { parkingTurnModel } from '~/models/parkingTurnModel';
@@ -26,6 +26,34 @@ const createVehicle = async (licenePlate, type) => {
   }
 };
 
+const isActive = async (licenePlate, idUser) => {
+  // const data = {
+  //   licenePlate : licenePlate,
+  //   type : type,
+  // }
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const vehicle = await vehicleModel.findOneByLicenePlate(licenePlate)
+    if(vehicle.driverId != null){
+      throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, 'Xe đã có chủ', 'Not Deleted', 'BR_vihicle_4');
+    }
+    const driver = await personModel.findByID(idUser)
+    if(driver.driver.arrayvehicleId.some(id => id.equals(new ObjectId(vehicle._id))) == false){
+      throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, 'Chủ xe không có xe này', 'Not Deleted', 'BR_vihicle_4');
+    }
+    const isActive = await vehicleModel.isActive(licenePlate, idUser)
+    if (isActive.acknowledged == false) {
+      throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, 'Xe tạo không thành công', 'Not Deleted', 'BR_vihicle_4');
+    }
+    return isActive;
+  } catch (error) {
+    if (error.type && error.code)
+      throw new ApiError(error.statusCode, error.message, error.type, error.code);
+    else throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
+  }
+};
+
 export const vehicleService = {
   createVehicle,
+  isActive,
 };
