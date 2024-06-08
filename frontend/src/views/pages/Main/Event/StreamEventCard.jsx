462a41fb61bf16ce189a822500c5428b601468d0
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2024-06-08 10:12:11 +0700

    temp 2

--- frontend/src/views/pages/Main/Event/StreamEventCard.jsx
+++ frontend/src/views/pages/Main/Event/StreamEventCard.jsx
@@ -1,31 +1,39 @@
 import React, {useState} from "react";
-import {Button, Input, Typography} from "antd";
+import {Button, Divider, Input, Radio, Typography} from "antd";
 import {useQuery} from "@tanstack/react-query";
 import {MediaServerApi} from "~/api";
 
 export const StreamEventCard = () => {
     const [stream, setStream] = useState();
-    const {data, refetch} = useQuery({
-        queryKey: ['Get', 'Data', 'FromStream'], queryFn: async () => {
+    const {data: {licenses = [], image}, refetch} = useQuery({
+        initialData: {}, queryKey: ['Get', 'Data', 'FromStream'], queryFn: async () => {
             let rs;
             try {
-                const [data1, data2] = await Promise.all(MediaServerApi.getLicensesFromStream(), MediaServerApi.captureImageFromStream(),);
+                const [data1, data2] = await Promise.allSettled(MediaServerApi.getLicensesFromStream(), MediaServerApi.captureImageFromStream(),);
                 console.log(data1, data2);
                 rs = {
-                    licenses: data1.result || [],
+                    licenses: data1.result || [], image: data2,
                 }
             } catch {
 
             }
             return rs;
         }
-    })
+    });
 
     return (<div className={'w-100'}>
         <img width={400} src={stream}
              alt={'No Img'}/>
-        <Typography.Title level={5}>Stream: {stream}</Typography.Title>
+        <Typography.Title level={5}>Stream đã nhập: {stream}</Typography.Title>
         <Input value={stream} onChange={(e) => setStream(e.target.value)}/>
+        <Divider/>
         <Button onClick={() => refetch()}>Cắt hình</Button>
+        <Divider/>
+        <Typography.Title level={5}>Các biển số xe được nhận diện:</Typography.Title>
+        <Radio.Group options={licenses.map(el => {
+            return {
+                label: el, value: el,
+            }
+        })}/>
     </div>)
 }
