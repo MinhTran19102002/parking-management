1c19410e96a9a0d8393e1d70908ee596a9f5a5b7
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2024-06-08 11:16:30 +0700

    fetch

--- frontend/src/api/Collections/MediaServerApi.js
+++ frontend/src/api/Collections/MediaServerApi.js
@@ -1,19 +1,27 @@
-import {GET, POST} from '../axiosMethods';
+import { GET, POST } from '../axiosMethods';
 
 const DOMAIN = import.meta.env.VITE_DOMAIN_MEDIA;
 
 export default {
-    captureImageFromStream: () => {
-        const url = `${DOMAIN}/imagecap`;
-        return GET({
-            url
-        })
-    },
+  captureImageFromStream: () => {
+    const url = `${DOMAIN}/imagecap`;
+    return fetch({
+      url,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  },
 
-    getLicensesFromStream: () => {
-        const url = `${DOMAIN}/licenseS`;
-        return GET({
-            url
-        })
-    }
-}
+  getLicensesFromStream: () => {
+    const url = `${DOMAIN}/licenseS`;
+    return fetch({
+      url,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+};
