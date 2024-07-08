1f330b473de8aa305c71b6ad898a2d03fcc4c70c
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2024-07-09 00:53:07 +0700

    point

--- frontend/src/views/components/Card/ChartSetting.jsx
+++ frontend/src/views/components/Card/ChartSetting.jsx
@@ -1,37 +1,25 @@
-import { Col, Form, Popover, Row, Switch, Typography } from "antd";
-import React from "react";
-import { MoreOutlined } from "@ant-design/icons";
-import { Button } from "antd";
+import { Col, Form, Popover, Row, Switch, Typography } from 'antd';
+import React from 'react';
+import { MoreOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import { useTranslation } from 'react-i18next';
 
 function ChartSetting({ onChangeSetting, initialValues }) {
+  const { t: lag } = useTranslation();
   return (
     <Popover
-      title={"Cài đặt"}
-      content={
-        <SettingForm
-          initialValues={initialValues}
-          onChangeSetting={onChangeSetting}
-        />
-      }
-    >
-      <Button
-        id="btnSettingCard"
-        icon={<MoreOutlined />}
-        size={"small"}
-        type="text"
-      />
+      title={lag('common:setting')}
+      content={<SettingForm initialValues={initialValues} onChangeSetting={onChangeSetting} />}>
+      <Button id="btnSettingCard" icon={<MoreOutlined />} size={'small'} type="text" />
     </Popover>
   );
 }
 
 const SettingForm = ({ onChangeSetting, initialValues }) => {
+  const { t: lag } = useTranslation();
   return (
     <Form initialValues={{ ...initialValues }} onValuesChange={onChangeSetting}>
-      <Form.Item
-        name={"isLabel"}
-        label="Hiển thị giá trị điểm"
-        valuePropName="checked"
-      >
+      <Form.Item name={'isLabel'} label={lag('common:popup:pointSetting')} valuePropName="checked">
         <Switch size="small" />
       </Form.Item>
     </Form>
