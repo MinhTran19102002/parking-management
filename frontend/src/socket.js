9a631010599f42988744524f6f5b09df3898e1a5
MinhTran19102002 <20110677@student.hcmute.edu.vn>
2024-07-12 22:31:47 +0700

    fix socket2

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 const socket = io(import.meta.env.VITE_DOMAIN1, {
   path: '/api',
   withCredentials: true, // include credentials when making cross-origin requests
-  transports: ['websocket'] // use only WebSocket transport (optional)
+  transports: ['websocket', 'polling'] // use only WebSocket transport (optional)
 });
 
 export default socket;
