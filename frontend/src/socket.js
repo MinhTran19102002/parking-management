7a69856f5c278c9f0fff3dd83f8a124989b9013e
MinhTran19102002 <20110677@student.hcmute.edu.vn>
2024-07-13 10:26:02 +0700

    socket*3

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -4,9 +4,9 @@ const socket = io(import.meta.env.VITE_DOMAIN_WS, {
   // path: "/my-custom-path/",
   // withCredentials: true, // include credentials when making cross-origin requests
   transports: ['websocket'], // use only WebSocket transport (optional)
-  secure: true,
-  reconnect: true,
-  rejectUnauthorized: false, // only if you are dealing with self-signed certificates
+  // secure: true,
+  // reconnect: true,
+  // rejectUnauthorized: false, // only if you are dealing with self-signed certificates
 });
 
 export default socket;
